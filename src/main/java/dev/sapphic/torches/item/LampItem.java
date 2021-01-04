3f7fbce507e0b553b4005df1ccfc7d3093a0c1ce
Chloe Dawn <chloe@sapphic.dev>
2021-01-04 04:12:14 +0000

    Extract lamp item suffix function to method reference

--- src/main/java/dev/sapphic/torches/item/LampItem.java
+++ src/main/java/dev/sapphic/torches/item/LampItem.java
@@ -3,11 +3,16 @@ package dev.sapphic.torches.item;
 import dev.sapphic.torches.block.LampBlock;
 import net.minecraft.block.Block;
 import net.minecraft.item.ItemMultiTexture;
+import net.minecraft.item.ItemStack;
 
 public class LampItem extends ItemMultiTexture {
   private static final LampBlock.Type[] TYPES = LampBlock.Type.values();
 
   public LampItem(final Block block) {
-    super(block, block, stack -> TYPES[stack.getMetadata() % TYPES.length].getName());
+    super(block, block, LampItem::getType);
+  }
+
+  private static String getType(final ItemStack stack) {
+    return TYPES[stack.getMetadata() % TYPES.length].getName();
   }
 }
